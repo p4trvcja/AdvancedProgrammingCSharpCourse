202c878780f937601150bd9b49321c30c306d245
Patrycja Potoczek <115663269+p4trvcja@users.noreply.github.com>
2024-04-06 09:21:38 +0200

    Update Program.cs

--- lab5/Directory_Watcher/Program.cs
+++ lab5/Directory_Watcher/Program.cs
@@ -1,6 +1,6 @@
 ﻿public class Program {
     public static void Main(string[] args) {
-        DirectoryWatcher directoryWatcher = new DirectoryWatcher("C:\\Users\\Patrycja\\OneDrive\\studia\\semestr4\\ProgramowanieZaawansowane2\\lab5\\Directory_Watcher");
+        DirectoryWatcher directoryWatcher = new DirectoryWatcher(".\\");
         Thread thread = new Thread(new ThreadStart(directoryWatcher.Start));
         thread.Start();
     }
