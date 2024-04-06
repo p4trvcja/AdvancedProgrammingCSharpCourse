a062fde5b0a6cef13d62f1b8cddde76dc0f2d00d
Patrycja Potoczek <115663269+p4trvcja@users.noreply.github.com>
2024-04-06 09:24:25 +0200

    Update Threads.cs

--- lab5/Threads/Threads.cs
+++ lab5/Threads/Threads.cs
@@ -1,29 +1,35 @@
 public class SingleThread {
-    public int threadId = 0;
+    public long threadId = 0;
     Threads parent;
-    public SingleThread(Threads rodzic, int numer) {
-        this.parent = rodzic;
-        this.threadId = numer;
+    
+    public SingleThread(Threads parent_, long threadId_) {
+        this.parent = parent_;
+        this.threadId = threadId_;
     }
+    
     public void Start() {
         Thread.Sleep(100);
         System.Console.WriteLine($"Thread {threadId} started.");
         Interlocked.Increment(ref parent.activeThreads);
+        
         Console.WriteLine($"Thread {threadId} is waiting for a signal.");
         Thread.Sleep(100);
         parent.ewhChild.WaitOne();
+        
         Interlocked.Decrement(ref parent.activeThreads);
         
         parent.ewhParent.Set();
         Console.WriteLine($"Thread {threadId} finished working.");
     }
 }
+
 public class Threads {
     public long activeThreads = 0;
     public long numberOfThreads;
     
     public EventWaitHandle ewhChild;
     public EventWaitHandle ewhParent;
+    
     public Threads(long numberOfThreads_) {
         numberOfThreads = numberOfThreads_;
         ewhChild = new EventWaitHandle(false, EventResetMode.AutoReset);
@@ -31,7 +37,7 @@ public class Threads {
     }
 
     public void Start() {
-        for(int i = 0; i < numberOfThreads; i++) {
+        for(long i = 0; i < numberOfThreads; i++) {
             SingleThread s = new SingleThread(this, i);
             Thread t = new Thread(new ThreadStart(s.Start));
             t.Start();
